daca9a5785d8a8eafb9a3f23ffcdd1c5fa099bb5
xiangshilin <295755973@qq.com>
2022-04-13 11:30:18 +0800

    🦄 refactor: Update

--- app/service/project.js
+++ app/service/project.js
@@ -4,41 +4,55 @@ const download = require('download-git-repo');
 const utils = require('../utils/fileUtils');
 const fs = require('fs');
 const process = require('child_process');
-const octokit = new Octokit({ auth: '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' });
+const octokit = new Octokit({ auth: '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' });
 
 function downloadFunc(downloadRepoUrl, temp_dest) {
   return new Promise(async (resolve, reject) => {
-    console.log(downloadRepoUrl);
-    download('zhuqitao/coco-template', temp_dest,  (err) => {
+    console.log('downloadRepoUrl', downloadRepoUrl);
+    console.log('temp_dest', temp_dest);
+    download(downloadRepoUrl + '#main', temp_dest, (err) => {
       if (err) {
         console.log(err);
-        reject('请求模板下载失败');
+        reject('template dowload fail!');
       } else {
-        resolve('请求模板下载成功');
+        resolve('template dowload success!');
       }
     })
   });
 }
 
 async function release(repoUrl, repoName) {
+  console.log('repoUrl', repoUrl)
+  console.log('repoName', repoName)
   try {
     process.execSync(
-      `cd static/${repoName}/dist &&
-       git init &&
-        git remote add origin ${repoUrl} &&
-       git add -A &&
-       git commit -m 'deploy' &&
-       git push -f ${repoUrl} master:gh-pages &&
-       cd -`
+      [
+        `cd static/${repoName}/dist`,
+        `git init`,
+        `git checkout -b gh-pages`,
+        `git remote add origin ${repoUrl}`,
+        `git add -A`,
+        `git commit -m 'deploy'`,
+        `git push -f origin gh-pages`,
+        `cd -` // 回到最初目录 liunx
+      ].join(' && ')
     )
-  }  catch (e) {
-    console.log(e);
+  } catch (e) {
+    console.log('error');
   } finally {
-    process.exec(`cd static && rm -rf ${repoName}`);
+    process.exec([
+      `cd static`,
+      `rm -rf ${repoName}`, // linux
+      // `rd /s /q ${repoName} ` // window
+    ].join(' && '), error => {
+      if (error) {
+        console.log('清除模板失败！', error)
+      }
+    })
   }
 }
 
-async function renderTpl({templateGit, name: repoName, data, repoUrl, templateConfig}) {
+async function renderTpl({ templateGit, name: repoName, data, repoUrl, templateConfig }) {
   if (!(await utils.existOrNot('./static'))) {
     await utils.mkdirFolder('static');
   }
@@ -70,22 +84,42 @@ async function renderTpl({templateGit, name: repoName, data, repoUrl, templateCo
   return Promise.resolve({});
 }
 
+async function isExistProject(owner, repo) {
+  try {
+    const { data } = await octokit.request('GET /repos/{owner}/{repo}', {
+      owner,
+      repo
+    })
+    return data
+  } catch (error) {
+    return false
+  }
+}
+
 class ProjectService extends Service {
   async createProject(config) {
-    // todo 判断是否已经存在项目，存在则不创建
-    // coco-h5 替换成创建的 organizations name
-    const {data: {id, ssh_url}} = await octokit.request('POST /orgs/coco-h5/repos', {
-      org: 'coco-h5',
-      name: config.name
-    });
+    let id, ssh_url;
+    // 判断是否已经存在项目，存在则不创建
+    const data = await isExistProject('mumu-page', config.name)
+    if (data) {
+      id = data.id
+      ssh_url = data.ssh_url
+    } else {
+      // 创建项目
+      const { data } = await octokit.request('POST /orgs/{org}/repos', {
+        org: 'mumu-page',
+        name: config.name
+      });
+      id = data.id
+      ssh_url = data.ssh_url
+    }
 
     await renderTpl({
       ...config,
       repoUrl: ssh_url
     });
-    return {id, ssh_url}
+    return { id, ssh_url }
   }
-
 }
 
 module.exports = ProjectService;
